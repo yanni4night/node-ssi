0bb059840f1de8d48736d4b38ea30beea1a9f83c
Nathan Friedly <nathan@nfriedly.com>
2015-10-08 14:48:56 -0400

    fix to avoid errors when a var is set to ""

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var async = require('async');
 
 var syntaxReg = /<!--#([^\r\n]+?)-->/mg;
 var includeFileReg = /<!--#\s*include\s+(file|virtual)=(['"])([^\r\n]+?)\2\s*-->/;
-var setVarReg = /<!--#\s*set\s+var=(['"])([^\r\n]+?)\1\s+value=(['"])([^\r\n]+?)\3\s*-->/;
+var setVarReg = /<!--#\s*set\s+var=(['"])([^\r\n]+?)\1\s+value=(['"])([^\r\n]*?)\3\s*-->/;
 var echoReg = /<!--#\s*echo\s+var=(['"])([^\r\n]+?)\1(\s+default=(['"])([^\r\n]+?)\4)?\s*-->/;
 var ifReg = /<!--#\s*if\s+expr=(['"])([^\r\n]+?)\1\s*-->/;
 var elifReg = /<!--#\s*elif\s+expr=(['"])([^\r\n]+?)\1\s*-->/;
